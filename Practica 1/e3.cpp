c6d355daf656272b579b35c4916f8938fbf046c6
Andres Carrasco Q <38218181+andritowmega@users.noreply.github.com>
2018-04-18 17:28:06 -0500

    Update e3.cpp

--- Practica 1/e3.cpp
+++ Practica 1/e3.cpp
@@ -1,23 +1,30 @@
 #include <iostream>
 
 using namespace std;
-int tamStr(char * ps){
-   if (*ps == '\0')
-        return 0;
-    return 1 + tamStr(ps + 1);
-}
-int tamStri(char * ps){
-
-    int tam = 0;
-   while (*(ps++) != '\0')
-       tam++;
-
-   return tam;
+void insertion_sort(int s[], int n)
+{
+  int i,tmp;
+  for (i=1; i<n; i++) {
+    while ((i>0) && (s[i] < s[i-1])) {//Verificamos a partir de la segunda posicion y vamos preguntando si el
+                                        //numero actual es menor que el anterior para intercambiarlos
+        tmp = s[i];
+        s[i] = s[i-1];
+        s[i-1] = tmp;
+        i--;                      //despues retrocedemos una posicion para seguir comprobando
+    }
+  }
 }
 int main()
 {
-    char s[] = "Hola Mundo";
-    cout << tamStr(s) << endl;
-    cout << tamStri(s) << endl;
-    return 0;
+    int vect[10] = {9,5,7,8,4,3,2,5,6,1};
+    for (int i=0;i<10;i++)
+    {
+        cout<<vect[i];
+    }
+    cout<<' '<<endl;
+    insertion_sort(vect,10);
+    for (int i=0;i<10;i++)
+    {
+        cout<<vect[i];
+    }
 }
